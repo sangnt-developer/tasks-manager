4709efe5724a4d56cfd93522e6e909f3d7f79bb4
Sang Nguyen - Home <sang.nguyen@orientsoftware.com>
2020-03-01 15:39:31 +0700

    Test github rule for master branch

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+//test github
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
